833fc17bc330f0733175b8b1760796af5a9ca9f6
Emmanuel <rivera128@live.com>
2021-02-15 16:09:13 -0600

    renamed a few things on signing.erb

--- app/controllers/bill_controller.rb
+++ app/controllers/bill_controller.rb
@@ -1,4 +1,8 @@
 require './config/environment'
 
 class BillController < Sinatra::Base
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+      end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,22 @@
 require './config/environment'
 
 class UserController < Sinatra::Base
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+      end
+
+      get '/signin' do 
+        erb :"users/signin"
+      end
+
+      post '/homepage' do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "users/homepage"
+        else
+            redirect "/signin"
+        end
+      end
 end
