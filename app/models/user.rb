ef21cc4674d3e24bbb87e5d86dfbd8e0ba635018
Emmanuel <rivera128@live.com>
2021-03-03 23:14:42 -0600

    added image for background

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,3 @@ class User < ActiveRecord::Base
     validates :username, uniqueness: true, length: {in: 4..25}, exclusion: {in: %w(admin superadmin user)}
     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\Z/i}
 end
-#testing
